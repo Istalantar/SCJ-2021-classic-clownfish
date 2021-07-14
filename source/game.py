d82f333ad484b1d5809806e849335e9ef30b1e46
Robin Cunningham <robinmichaelcunningham@gmail.com>
2021-07-14 12:07:50 +0200

    Implement TUI main menu
    - Main page
    - Select already saved puzzle
    - Select new puzzles from `.jpg` / `.png` files

---
+++ source/game.py
@@ -0,0 +1,26 @@
+from sliding_puzzle import Puzzle
+
+
+class Game:
+    """Game class"""
+
+    def __init__(self, image: str, difficulty: int = 1):
+        """Init function
+
+        Args:
+            image (str): image path
+            difficulty (int, optional): integer ranging from 1 to 5. Defaults to 1.
+        """
+        if difficulty > 5:
+            difficulty = 5
+        if difficulty < 1:
+            difficulty = 1
+
+        # Convert image to ASCII
+        # self.ascii = toAscii(image)
+        with open('../resources/ascii_art/amogus.txt', 'r') as f:
+            self.ascii = f.read()
+        # Need to adjust the dimensions for sliding_puzzle.Puzzle
+        vertical = 0
+        horizontal = 0
+        self.puzzle = Puzzle(self.ascii, vertical, horizontal)
