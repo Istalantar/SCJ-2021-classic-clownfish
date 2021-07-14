7548d263ef99236314de84086871a8b0150b55fb
Tobi DEGNON <tobidegnon@protonmail.com>
2021-07-14 10:29:43 +0100

    implement sliding mechanics and puzzle shuffle

--- source/sliding_puzzle.py
+++ source/sliding_puzzle.py
@@ -1,10 +1,21 @@
 from typing import List
+import random
+from dataclasses import dataclass
 
 
 def walk(string: str, step: int) -> str:
     """Helper generator to iterate over a string with steps"""
     for i in range(0, len(string), step):
         yield string[i:i+step]
+
+
+@dataclass
+class PiecePosition:
+    """A piece coordinates"""
+
+    x: int
+    y: int
+    index: int
 
 
 class PuzzlePiece:
@@ -26,7 +37,7 @@ class PuzzlePiece:
         self._data = []
 
     def __repr__(self) -> str:
-        return f'<PuzzlePiece index={self.index} width={self.width} height={self.height} empty={self.empty}>'
+        return f'<PuzzlePiece index={self.index} width={self.width} height={self.height} empty={self.is_empty}>'
 
     def __str__(self) -> str:
         return '\n'.join(self.data)
@@ -37,10 +48,10 @@ class PuzzlePiece:
     @property
     def data(self) -> List[str]:
         """List of lines of the image."""
-        return self._data or [' ' * self.width] * self.height
+        return self._data or [" " * self.width] * self.height
 
     @property
-    def empty(self) -> bool:
+    def is_empty(self) -> bool:
         """Whether this piece is empty (not filled)."""
         return bool(self)
 
@@ -48,20 +59,23 @@ class PuzzlePiece:
         """Append a string to the end of the data list."""
         self._data.append(string)
 
+    def clear(self):
+        self._data = list()
+
 
 class Puzzle:
     """Main puzzle game class, taking care of the whole game logic."""
 
-    pieces: List[List[PuzzlePiece]]
+    rows: List[List[PuzzlePiece]]
 
-    __slots__ = ('pieces',)
+    __slots__ = ('rows',)
 
     def __init__(self, image: str, horizontal: int, vertical: int) -> None:
         """Initialize the Puzzle with an image and size to split it by.
 
         :param image: An ASCII string that will be split
-        :param horizontal: Amount of horizontal pieces
-        :param vertical: Amount of vertical pieces
+        :param horizontal: Amount of horizontal rows
+        :param vertical: Amount of vertical rows
         """
         data = image.split('\n')
 
@@ -83,7 +97,7 @@ class Puzzle:
         # Even though width and height have no decimals, they are still floats
         width, height = int(width), int(height)
 
-        pieces: List[List[PuzzlePiece]] = [
+        rows: List[List[PuzzlePiece]] = [
             [
                 # This will give each PuzzlePiece an index from 0 to (horizontal*vertical - 1)
                 PuzzlePiece(i, width, height) for i in range(v*horizontal, v*horizontal+horizontal)
@@ -92,18 +106,18 @@ class Puzzle:
 
         for i, line in enumerate(data):
             for j, column in enumerate(walk(line, width)):
-                pieces[i//height][j].append(column)
+                rows[i//height][j].append(column)
 
-        self.pieces = pieces
+        self.rows = rows
 
     def __repr__(self) -> str:
-        return f'<Puzzle solved={self.solved} pieces={self.pieces}>'
+        return f'<Puzzle solved={self.solved} rows={self.rows}>'
 
     @property
     def solved(self) -> bool:
         """Whether the puzzle is considered solved."""
         i = 0
-        for row in self.pieces:
+        for row in self.rows:
             for piece in row:
                 if piece.index != i:
                     return False
@@ -112,7 +126,7 @@ class Puzzle:
         return True
 
     def _join(self, row: List[PuzzlePiece]) -> List[str]:
-        """Join together a row of puzzle pieces into a complete list of lines."""
+        """Join together a row of puzzle rows into a complete list of lines."""
         res = []
         for i in range(len(row[0].data)):
             res.append('')
@@ -124,7 +138,84 @@ class Puzzle:
     def draw(self) -> str:
         """Draw the puzzle in its current state."""
         output = []
-        for row in self.pieces:
+        for row in self.rows:
             output.extend(self._join(row))
 
         return '\n'.join(output)
+
+    def shuffle(self):
+        for row in self.rows:
+            random.shuffle(row)
+        puzzle_heigth, puzzle_width = len(self.rows), len(self.rows[0])
+        # empty one of the rows
+        x = random.choice(range(puzzle_heigth))
+        y = random.choice(range(puzzle_width))
+        self.rows[x][y].clear()
+
+    def _get_empty_piece_position(self) -> PiecePosition:
+        x, y, index = [
+            (x, y, piece.index)
+            for x, row in enumerate(self.rows)
+            for y, piece in enumerate(row)
+            if piece._data == []
+        ][0]
+        return PiecePosition(x=x, y=y, index=index)
+
+    def move_down(
+        self,
+    ):
+        empty_pos = self._get_empty_piece_position()
+        # return if empty piece is on the first row
+        top_row = self.rows[0]
+        if empty_pos.index in [piece.index for piece in top_row]:
+            return
+        # swap the empty piece with the target piece
+        self.rows[empty_pos.x][empty_pos.y], self.rows[empty_pos.x - 1][empty_pos.y] = (
+            self.rows[empty_pos.x - 1][empty_pos.y],
+            self.rows[empty_pos.x][empty_pos.y],
+        )
+
+    def move_up(
+        self,
+    ):
+        empty_pos = self._get_empty_piece_position()
+        # return if empty piece is on the last row
+        last_row = self.rows[-1]
+        if empty_pos.index in [piece.index for piece in last_row]:
+            return
+        # swap the empty piece with the target piece
+        self.rows[empty_pos.x][empty_pos.y], self.rows[empty_pos.x + 1][empty_pos.y] = (
+            self.rows[empty_pos.x + 1][empty_pos.y],
+            self.rows[empty_pos.x][empty_pos.y],
+        )
+
+    def move_right(
+        self,
+    ):
+        empty_pos = self._get_empty_piece_position()
+        # return if empty piece is on the last column
+        puzzle_width = len(self.rows[0])
+        last_column_y = puzzle_width - 1
+        last_column = [piece for row in self.rows for j, piece in enumerate(row) if j == last_column_y]
+        if empty_pos.index in [piece.index for piece in last_column]:
+            return
+        # swap the empty piece with the target piece
+        self.rows[empty_pos.x][empty_pos.y], self.rows[empty_pos.x][empty_pos.y + 1] = (
+            self.rows[empty_pos.x][empty_pos.y + 1],
+            self.rows[empty_pos.x][empty_pos.y],
+        )
+
+    def move_left(
+        self,
+    ):
+        empty_pos = self._get_empty_piece_position()
+        # return if empty piece is on the first column
+        first_column = [piece for row in self.rows for j, piece in enumerate(row) if j == 0]
+        if empty_pos.index in [piece.index for piece in first_column]:
+            return
+        # swap the empty piece with the target piece
+        self.rows[empty_pos.x][empty_pos.y], self.rows[empty_pos.x][empty_pos.y - 1] = (
+            self.rows[empty_pos.x][empty_pos.y - 1],
+            self.rows[empty_pos.x][empty_pos.y],
+        )
+
