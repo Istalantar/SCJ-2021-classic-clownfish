1945916aad37a8813119ce0ecb63fe754690e233
Istalantar <istalantar@gmail.com>
2021-07-16 23:23:34 +0200

    Update initial rendering for the game menu

--- source/game.py
+++ source/game.py
@@ -1,9 +1,8 @@
-
 from typing import Union
 
 from blessed import Terminal
 from images import Image
-from menus.utils import State, Menu
+from menus.utils import Menu, State
 from sliding_puzzle import Puzzle
 
 
@@ -20,15 +19,24 @@ class Game(Menu):
 
         difficulty = min(5, max(difficulty, 1))
 
-        self.image = Image(image, 60)
+        self.image = Image(image, 90)
 
-        self.puzzle = Puzzle(self.image.generate_ascii, 2, 2)
+        self.puzzle = Puzzle(self.image.generate_ascii, 3, 3)
         self.puzzle.shuffle()
 
     def render(self, term: Terminal) -> str:
-        """Render the start-menu."""
-        rendered = term.move_y(3)  # just some spacing from the top for now
-        rendered += self.puzzle.draw()
+        """Render the game-menu."""
+        rendered = term.move_y(4)  # just a spacing of four
+
+        lines = self.puzzle.draw().split('\n')
+
+        for line in lines:
+            rendered += term.center(line)
+
+        rendered += term.move_down(2)
+        rendered += term.center('Use the arrow keys to move the pieces')
+        rendered += term.center('Hit TAB + Enter to go back to the start menu')
+        rendered += ' ' * 4 + (term.black_on_white('Exit') if self.selected == 1 else 'Exit')
 
         return rendered
 
@@ -44,10 +52,18 @@ class Game(Menu):
             self.puzzle.move_right()
         elif key == term.KEY_TAB:
             self.selected = int(not self.selected)
+        else:
+            print('you hit an unsupported key')
 
     def click(self, term: Terminal) -> State:
         """Handle a enter-press."""
         if self.selected == 1:
             return State.start
         else:
-            return self.state
+            return State.playing
+
+
+if __name__ == '__main__':
+    game = Game('../resources/phoenix.jpg')
+    t_term = Terminal()
+    print(game.render(t_term))
