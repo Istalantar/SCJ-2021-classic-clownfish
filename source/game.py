e62f3cc0740410ca3c76432c61fa3d6d9eb4f0aa
Bluenix <bluenixdev@gmail.com>
2021-07-16 17:40:47 +0200

    Change Game to conform to Menu missing selected
    Fixes several other issues as well, such as movement being inverted and not correctly starting.

--- source/game.py
+++ source/game.py
@@ -2,56 +2,52 @@
 from typing import Union
 
 from blessed import Terminal
-from menus.utils import State
+from images import Image
+from menus.utils import State, Menu
 from sliding_puzzle import Puzzle
 
 
-class Game:
-    """Game class"""
+class Game(Menu):
+    """Game menu where the user is playing."""
 
     def __init__(self, image: str, difficulty: int = 1):
         """Init function
 
-        :param image: image path
+        :param image: Image path
         :param difficulty: integer ranging from 1 to 5. Defaults to 1.
         """
-        self.state = State.game
+        self.selected = 0
 
         difficulty = min(5, max(difficulty, 1))
 
-        with open(image, 'r') as f:
-            self.ascii = f.read()
+        self.image = Image(image, 60)
 
-        # Need to adjust the dimensions for sliding_puzzle.Puzzle
-        vertical = 0
-        horizontal = 0
-        self.puzzle = Puzzle(self.ascii, vertical, horizontal)
+        self.puzzle = Puzzle(self.image.generate_ascii, 2, 2)
+        self.puzzle.shuffle()
 
-    def render(self, term: Terminal, selected: int) -> str:
+    def render(self, term: Terminal) -> str:
         """Render the start-menu."""
         rendered = term.move_y(3)  # just some spacing from the top for now
         rendered += self.puzzle.draw()
 
         return rendered
 
-    def kinput(self, term: Terminal, selected: int, key: Union[int, None]) -> int:
+    def kinput(self, term: Terminal, key: Union[int, None]) -> None:
         """Handle keyboard input (what button is selected)."""
         if key == term.KEY_UP:
-            self.puzzle.move_up()
-        elif key == term.KEY_RIGHT:
-            self.puzzle.move_right()
-        elif key == term.KEY_DOWN:
             self.puzzle.move_down()
-        elif key == term.KEY_LEFT:
+        elif key == term.KEY_RIGHT:
             self.puzzle.move_left()
+        elif key == term.KEY_DOWN:
+            self.puzzle.move_up()
+        elif key == term.KEY_LEFT:
+            self.puzzle.move_right()
         elif key == term.KEY_TAB:
-            selected = 1
+            self.selected = int(not self.selected)
 
-        return selected
-
-    def click(self, term: Terminal, selected: int) -> State:
+    def click(self, term: Terminal) -> State:
         """Handle a enter-press."""
-        if selected == 1:
+        if self.selected == 1:
             return State.start
         else:
             return self.state
