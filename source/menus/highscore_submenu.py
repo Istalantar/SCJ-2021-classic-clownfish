842b2d8123ccca2fff8818d023fb3265ab87c280
Robin Cunningham <68775372+robincunningham2@users.noreply.github.com>
2021-07-17 18:22:29 +0200

    update to highscore menu

---
+++ source/menus/highscore_submenu.py
@@ -0,0 +1,49 @@
+from typing import TYPE_CHECKING
+
+from blessed import Terminal as Interface
+from game import Game
+from highscore import Highscore
+
+from .utils import Menu
+
+if TYPE_CHECKING:
+    # Interface is a subclass of Terminal, importing it directly would cause circular imports
+    from ..main import Interface  # noqa: F811
+
+
+class HighScoreSubMenu(Menu):
+    """The highscore sub-menu."""
+
+    def __init__(self, puzzle: str) -> None:
+        self.selected = 0
+        self.puzzle = puzzle
+        self.highscores = []
+        highscores = Highscore().highscore
+
+        for hs in highscores:
+            if hs['Puzzle'] == self.puzzle and hs not in self.highscores:
+                self.highscores.append(hs)
+
+        self.highscores.sort(key=lambda x: x['Moves'] + x['Time'])
+
+    def render(self, term: Interface) -> str:
+        """Render the highscores."""
+        rendered = []
+        rendered.append(term.move_down(1))
+        for i, highscore in enumerate(self.highscores):
+            color = term.webgrey
+            if i == 0:
+                color = term.color_rgb(215, 190, 105)
+            if i == 1:
+                color = term.color_rgb(192, 192, 192)
+            if i == 2:
+                color = term.color_rgb(169, 113, 66)
+
+            rendered.append(term.move_x(4) + color(f'{i + 1}. {highscore["Name"]}'))
+
+        rendered.append(term.move_yx(term.height - 3, 4) + 'Press enter to play this puzzle')
+        return '\n'.join(rendered)
+
+    def click(self, term: Interface) -> Menu:
+        """Handle enter key presses."""
+        return Game(self.puzzle)  # TODO: fetch puzzle from name
