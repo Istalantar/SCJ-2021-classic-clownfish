e8bbcbe3e2caf34669ecad6ea29e297913265202
Robin Cunningham <68775372+robincunningham2@users.noreply.github.com>
2021-07-17 12:27:54 +0200

    Create PopupMessage Menu (#9)
    This is currently used in ChooseFile when catching unrecognied images so that the user can try again.

--- source/menus/utils.py
+++ source/menus/utils.py
@@ -1,4 +1,5 @@
-from typing import TYPE_CHECKING, Protocol
+import math
+from typing import TYPE_CHECKING, Callable, Protocol
 
 from blessed import Terminal as Interface
 from blessed.keyboard import Keystroke
@@ -26,6 +27,38 @@ class Menu(Protocol):
     def click(self, term: Interface) -> 'Menu':  # Forward reference
         """Handle ENTER presses, this method should return a menu instance."""
         ...
+
+
+class PopupMessage(Menu):
+    """Popup message class"""
+
+    def __init__(self, origin: Menu, message: str, color: Callable) -> None:
+        """Initialize the popup message"""
+        self.origin = origin
+        self.message = message
+        self.color = color
+
+    def render(self, term: Interface) -> str:
+        """Render the message waiting for the user to continue"""
+        width = min(term.width - 8, len(self.message) + 12) - 2
+        popup = [term.center('┌' + '—' * width + '┐')]
+        popup.append(term.center('│' + ' ' * width + '│'))
+
+        for item in (self.message, 'Press enter to continue'):
+            popup.append(term.center(
+                '│' + math.floor((width - len(item)) / 2) * ' '
+                + self.color(item)
+                + math.ceil((width - len(item)) / 2) * ' ' + '│'
+            ))
+
+            popup.append(term.center('│' + ' ' * width + '│'))
+
+        popup.append(term.center('└' + '-' * width + '┘'))
+        return term.move_y(term.height // 2) + '\n'.join(popup)
+
+    def click(self, term: Interface) -> Menu:
+        """Handle a enter-press."""
+        return self.origin
 
 
 def set_string_length(string: str, length: int) -> str:
